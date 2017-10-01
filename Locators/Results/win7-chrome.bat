7ed31f57f718ecf74566b31a5cda105bf2b8df5b
Gulnara Abilova <abgulia@hotmail.com>
2017-10-01 21:49:42 +0200

    IfElse and Loop

--- Locators/Results/win7-chrome.bat
+++ Locators/Results/win7-chrome.bat
@@ -1,3 +1,3 @@
 @echo off
 cd C:\Users\GulnaraAbilovatesena\OneDrive - tesena s.r.o\development\RobotFramework\Locators
-call pybot -d Results Tests/MouseLocator.robot
+call pybot -d Results Tests/ElseIf.robot
